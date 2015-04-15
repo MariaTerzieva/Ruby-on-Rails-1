9d223daa76cb7a2a6e18c27ed9a84f2d056a2395
Maria Terzieva <mariya.g.terzieva@gmail.com>
2015-04-15 23:48:55 +0300

    Add title to the posts

--- week3/2-extend-the-blog/microblog/db/schema.rb
+++ week3/2-extend-the-blog/microblog/db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402171250) do
+ActiveRecord::Schema.define(version: 20150415204551) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
   create_table "tags", force: :cascade do |t|

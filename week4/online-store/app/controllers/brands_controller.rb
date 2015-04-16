9d545081e7edc3fc79b951f9984642f498a6f8aa
Maria Terzieva <mariya.g.terzieva@gmail.com>
2015-04-16 21:46:23 +0300

    Add controllers

---
+++ week4/online-store/app/controllers/brands_controller.rb
@@ -0,0 +1,28 @@
+class BrandsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destoy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def count
+  end
+
+  def range
+  end
+end

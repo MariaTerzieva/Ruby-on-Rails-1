8c05ea3fe41d6a5a5cdfb467673512708911eac6
Maria Terzieva <mariya.g.terzieva@gmail.com>
2015-04-27 21:50:26 +0300

    Implement lectures

--- week5/course-management/app/controllers/lectures_controller.rb
+++ week5/course-management/app/controllers/lectures_controller.rb
@@ -1,22 +1,47 @@
 class LecturesController < ApplicationController
   def index
+    @lectures = Lecture.all
   end
 
   def show
-  end
-
-  def create
+    @lecture = Lecture.find_by(id: params[:id])
   end
 
   def new
+    @lecture = Lecture.new
+  end
+
+  def create
+    @lecture = Lecture.new(lecture_params)
+
+    if @lecture.save
+      redirect_to @lecture
+    else
+      render :new
+    end
   end
 
   def delete
+    @lecture = Lecture.find_by(id: params[:id])
+    @lecture.destroy
+    redirect_to lectures_url
   end
 
   def edit
+    @lecture = Lecture.find_by(id: params[:id])
   end
 
   def update
+    @lecture = Lecture.find_by(id: params[:id])
+    if @lecture.update(lecture_params)
+      redirect_to @lecture
+    else
+      render :edit
+    end
   end
+
+  private
+    def lecture_params
+      params.require(:lecture).permit(:name, :text_body)
+    end
 end

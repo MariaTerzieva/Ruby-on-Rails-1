c0dd424875bba89b33f577ac28b6b88fe20ea17f
Maria Terzieva <mariya.g.terzieva@gmail.com>
2015-04-27 20:52:55 +0300

    Add controllers

---
+++ week5/course-management/app/controllers/lectures_controller.rb
@@ -0,0 +1,22 @@
+class LecturesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def delete
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
---
+++ week5/course-management/app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationCntroller
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def delete
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
